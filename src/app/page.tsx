a058e9055fc2c74bbf11fe6d20a7b49761ff0e61
a2774 <akumar64587@gmail.com>
2025-08-15 17:18:40 +0530

    make responce api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Banner from "@/app/Banner";
+import ProductList from "./(home)/components/ProductList";
 // import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE}/GetAllCustomer`);
+
+
+  if (!response.ok) {
+    throw new Error("Data is not fetching");
+  }
+
+  const data = await response.json();
+  console.log("API Data:", data);
+
   return (
-  <>
- <Banner/>
-  </>
+    <>
+      <Banner />
+      <ProductList />
+    </>
   );
 }
